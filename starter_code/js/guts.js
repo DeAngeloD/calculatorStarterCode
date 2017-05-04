3246165bc6a54f8c13473cd4112bd18022c8540f
DeAngeloD <lildeckler@gmail.com>
2017-05-04 16:38:09 -0700

    Update guts.js

--- starter_code/js/guts.js
+++ starter_code/js/guts.js
@@ -4,34 +4,57 @@
 
 
 function divide(x,y) {
-	//Write your code below this line
-
+	//Write your code below this line	
+	return x/y;
 }
+console.log(divide(10,2));
 
 function multiply(x,y) {
 
+return x*y;
 }
+console.log(multiply(10,2));
 
 function subtract(x,y) {
-
+return x-y;
 }
+console.log(subtract(10,2));
 
 function add(x,y) {
-
+return x+y;
 }
+console.log(add(10,2));
 
 function power(x,y) {
-
+return Math.pow(x,y);
 }
+console.log(power(9,2));
 
 function square_root(x) {
-
+return Math.sqrt(x);
 }
+console.log(square_root(36));
 
 function logBase10(x) {
-
+return Math.log10(x);
 }
+console.log(logBase10(100));
 
 function generateRandomNumber() {
-
+return Math.random();
 }
+console.log(generateRandomNumber());
+
+// BONUS: write assertions to test whether your functions work!
+// Here are some examples for divide(x,y), can you write more for the other functions?
+//
+// console.assert(divide(1,1) === 1, "1/1 should equal 1");
+function divided(x,y) {
+return x/y;
+}
+console.assert(divided(1,1) === 1, "1/2 should equal 1");
+// console.assert(divide(1,2) === 0.5, "1/2 should equal 0.5");
+function divided(x,y) {
+return x/y;
+}
+console.assert(divided(1,2) === 0.5, "1/2 should equal 0.5");
